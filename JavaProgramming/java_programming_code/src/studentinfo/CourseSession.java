955013dfc09db1844bd07ad4d504cb988ce89aa2
홍석준 <tjrwns0529@gmail.com>
2020-08-17 02:25:48 +0900

    Add lesson2

---
+++ JavaProgramming/java_programming_code/src/studentinfo/CourseSession.java
@@ -0,0 +1,55 @@
+package studentinfo;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class CourseSession {
+    private String department;
+    private String number;
+    private ArrayList<Student> students = new ArrayList<Student>();
+    private Date startDate;
+
+    CourseSession(String department, String number, Date startDate) {
+        this.department = department;
+        this.number = number;
+        this.startDate = startDate;
+    }
+
+    String getDepartment() {
+        return department;
+    }
+
+    String getNumber() {
+        return number;
+    }
+
+    int getNumberOfStudents() {
+        return students.size();
+    }
+
+    void enroll(Student student) {
+        students.add(student);
+    }
+
+    public ArrayList<Student> getAllStudents() {
+        return students;
+    }
+
+    public Student get(int index) {
+        return students.get(index);
+    }
+
+    public Date getEndDate() {
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(startDate);
+        int numberOfDays = 16 * 7 - 3;
+        calendar.add(Calendar.DAY_OF_YEAR, numberOfDays);
+        return calendar.getTime();
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+}
