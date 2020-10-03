550708d02ca96c4791f6c245f582c36adee70808
홍석준 <tjrwns0529@gmail.com>
2020-10-04 00:54:49 +0900

    Create ReportCard

---
+++ JavaProgramming/java_programming_code/src/sis/report/ReportCard.java
@@ -0,0 +1,37 @@
+package sis.report;
+
+import sis.studentinfo.Student;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class ReportCard {
+    static final String A_MESSAGE = "Excellent";
+    static final String B_MESSAGE = "Very good";
+    static final String C_MESSAGE = "Hmmm...";
+    static final String D_MESSAGE = "Yore not trying";
+    static final String F_MESSAGE = "Loser";
+
+    private Map<Student.Grade, String> messages = null;
+
+    public String getMessage(Student.Grade grade) {
+        return getMessages().get(grade);
+    }
+
+    public Map<Student.Grade, String> getMessages() {
+        if (messages == null)
+            loadMessages();
+        return messages;
+    }
+
+    public void loadMessages() {
+        messages =
+                new EnumMap<Student.Grade, String>(Student.Grade.class);
+        messages.put(Student.Grade.A, A_MESSAGE);
+        messages.put(Student.Grade.B, B_MESSAGE);
+        messages.put(Student.Grade.C, C_MESSAGE);
+        messages.put(Student.Grade.D, D_MESSAGE);
+        messages.put(Student.Grade.F, F_MESSAGE);
+    }
+
+}
--- JavaProgramming/java_programming_code/src/sis/studentinfo/Student.java
+++ JavaProgramming/java_programming_code/src/sis/studentinfo/Student.java
@@ -3,7 +3,7 @@ package sis.studentinfo;
 import java.util.ArrayList;
 
 public class Student {
-    enum Grade { A, B, C, D, F};
+    public enum Grade { A, B, C, D, F};
 
     static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
     static final String IN_STATE = "CO";
