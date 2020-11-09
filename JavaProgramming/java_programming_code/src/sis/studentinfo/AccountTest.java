cc70a03ba4deb6ab2469a60ff334e88299987141
홍석준 <tjrwns0529@gmail.com>
2020-11-09 23:21:43 +0900

    BigDecimal

---
+++ JavaProgramming/java_programming_code/src/sis/studentinfo/AccountTest.java
@@ -0,0 +1,22 @@
+package sis.studentinfo;
+
+import junit.framework.TestCase;
+
+import java.math.BigDecimal;
+
+public class AccountTest extends TestCase {
+    public void testTransactions() {
+        Account account = new Account();
+        account.credit(new BigDecimal("0.10"));
+        account.credit(new BigDecimal("11.00"));
+        assertEquals(new BigDecimal("11.10"), account.getBalance());
+    }
+
+    public void testTransactionAverage() {
+        Account account = new Account();
+        account.credit(new BigDecimal("0.10"));
+        account.credit(new BigDecimal("11.00"));
+        account.credit(new BigDecimal("2.99"));
+        assertEquals(new BigDecimal("4.70"), account.transactionAverage());
+    }
+}
