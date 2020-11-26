8d0cc24c5d9bc6e85da2808c97bb09476b27347c
hooong <tjrwns0529@gmail.com>
2020-11-26 22:23:40 +0900

    [INFO] Scanning for projects... [INFO] [INFO] ------------------------< com.hooong:diexample >------------------------ [INFO] Building diexample 1.0-SNAPSHOT [INFO] --------------------------------[ jar ]--------------------------------- [INFO] [INFO] --- maven-resources-plugin:2.6:resources (default-resources) @ diexample --- [INFO] Using 'UTF-8' encoding to copy filtered resources. [INFO] skip non existing resourceDirectory /Users/hooong/Desktop/TIL/The_Java/diexample/src/main/resources [INFO] [INFO] --- maven-compiler-plugin:3.1:compile (default-compile) @ diexample --- [INFO] Changes detected - recompiling the module! [INFO] Compiling 3 source files to /Users/hooong/Desktop/TIL/The_Java/diexample/target/classes [INFO] [INFO] --- maven-resources-plugin:2.6:testResources (default-testResources) @ diexample --- [INFO] Using 'UTF-8' encoding to copy filtered resources. [INFO] skip non existing resourceDirectory /Users/hooong/Desktop/TIL/The_Java/diexample/src/test/resources [INFO] [INFO] --- maven-compiler-plugin:3.1:testCompile (default-testCompile) @ diexample --- [INFO] Changes detected - recompiling the module! [INFO] Compiling 1 source file to /Users/hooong/Desktop/TIL/The_Java/diexample/target/test-classes [INFO] [INFO] --- maven-surefire-plugin:2.12.4:test (default-test) @ diexample --- [INFO] Surefire report directory: /Users/hooong/Desktop/TIL/The_Java/diexample/target/surefire-reports
    -------------------------------------------------------
     T E S T S
    -------------------------------------------------------
    Running com.hooong.AppTest
    Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.032 sec
    
    Results :
    
    Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
    
    [INFO]
    [INFO] --- maven-jar-plugin:2.4:jar (default-jar) @ diexample ---
    [INFO] Building jar: /Users/hooong/Desktop/TIL/The_Java/diexample/target/diexample-1.0-SNAPSHOT.jar
    [INFO]
    [INFO] --- maven-install-plugin:2.4:install (default-install) @ diexample ---
    [INFO] Installing /Users/hooong/Desktop/TIL/The_Java/diexample/target/diexample-1.0-SNAPSHOT.jar to /Users/hooong/.m2/repository/com/hooong/diexample/1.0-SNAPSHOT/diexample-1.0-SNAPSHOT.jar
    [INFO] Installing /Users/hooong/Desktop/TIL/The_Java/diexample/pom.xml to /Users/hooong/.m2/repository/com/hooong/diexample/1.0-SNAPSHOT/diexample-1.0-SNAPSHOT.pom
    [INFO] ------------------------------------------------------------------------
    [INFO] BUILD SUCCESS
    [INFO] ------------------------------------------------------------------------
    [INFO] Total time:  1.523 s
    [INFO] Finished at: 2020-11-26T22:23:40+09:00
    [INFO] ------------------------------------------------------------------------을 하고 다른 프로젝트에서 디펜던시로 가져다 쓰기

--- The_Java/diexample/src/main/java/com/hooong/AccountService.java
+++ The_Java/diexample/src/main/java/com/hooong/AccountService.java
@@ -1,2 +1,15 @@
-package com.hooong;public class AccountService {
+package com.hooong;
+
+import com.hooong.di.Inject;
+
+public class AccountService {
+
+    @Inject
+    AccountRepository accountRepository;
+
+    public void join() {
+        System.out.println("Service.join");
+        accountRepository.save();
+    }
+
 }
---
+++ The_Java/diexample/src/main/java/com/hooong/App.java
@@ -0,0 +1,15 @@
+package com.hooong;
+
+import com.hooong.di.ContainerService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        AccountService accountService = ContainerService.getObject(AccountService.class);
+        accountService.join();
+    }
+}
