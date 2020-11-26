0aaa3c1c51e606101f33a7f8170cb36d189459ee
hooong <tjrwns0529@gmail.com>
2020-11-26 22:01:44 +0900

    리플렉션-2, 클래스의 메소드 및 필드 수정 및 실행

--- The_Java/refactoring-example/src/main/java/com/hooong/MyAnnotation.java
+++ The_Java/refactoring-example/src/main/java/com/hooong/MyAnnotation.java
@@ -2,6 +2,7 @@ package com.hooong;
 
 import java.lang.annotation.*;
 
+// Retention으로 해당 애노테이션이 어디까지 살아남을 수 있는지를 지정할 수 있음.
 @Retention(RetentionPolicy.RUNTIME)
 //@Target({ElementType.TYPE, ElementType.FIELD})
 @Inherited
