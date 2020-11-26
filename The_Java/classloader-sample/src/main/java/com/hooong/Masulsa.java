4d62fe811b3de7f128568887afc9eba6a7c485f9
hooong <tjrwns0529@gmail.com>
2020-11-26 18:59:49 +0900

    -javaagent 사용하여 바이트 코드 조작

--- The_Java/classloader-sample/src/main/java/com/hooong/Masulsa.java
+++ The_Java/classloader-sample/src/main/java/com/hooong/Masulsa.java
@@ -12,15 +12,6 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Masulsa {
     public static void main(String[] args) throws IOException {
-        ClassLoader classLoader = Masulsa.class.getClassLoader();
-        TypePool typePool = TypePool.Default.of(classLoader);
-
-        new ByteBuddy().redefine(
-                typePool.describe("com.hooong.Moja").resolve(),
-                ClassFileLocator.ForClassLoader.of(classLoader))
-                .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
-                .make().saveIn(new File("/Users/hooong/Desktop/TIL/The_Java/classloader-sample/target/classes/"));
-
         System.out.println(new Moja().pullOut());
     }
 }
