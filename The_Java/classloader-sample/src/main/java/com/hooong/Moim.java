c859f461edf02a33d106cf982d42463c4bcd5855
hooong <tjrwns0529@gmail.com>
2020-11-26 16:54:12 +0900

    jacoco 실습

---
+++ The_Java/classloader-sample/src/main/java/com/hooong/Moim.java
@@ -0,0 +1,20 @@
+package com.hooong;
+
+public class Moim {
+
+    int maxNumberOfAttendees;
+
+    int numberOfEnrollment;
+
+    public boolean isEnrollmentFull() {
+        if (maxNumberOfAttendees == 0) {
+            return false;
+        }
+
+        if (numberOfEnrollment < maxNumberOfAttendees) {
+            return false;
+        }
+
+        return true;
+    }
+}
